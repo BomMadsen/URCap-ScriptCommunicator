7ac64f5d5d46468d0bb08755ab83888780b47545
jacobbsmadsen@gmail.com <jacobbsmadsen@gmail.com>
2018-11-12 16:12:51 +0100

    Configured for deployment to 3.7 URSim

--- com.jbm.urcap.sample.scriptCommunicator/src/main/java/com/jbm/urcap/sample/scriptCommunicator/impl/Activator.java
+++ com.jbm.urcap.sample.scriptCommunicator/src/main/java/com/jbm/urcap/sample/scriptCommunicator/impl/Activator.java
@@ -3,6 +3,8 @@ package com.jbm.urcap.sample.scriptCommunicator.impl;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
 
+import com.ur.urcap.api.contribution.installation.swing.SwingInstallationNodeService;
+
 /**
  * Hello world activator for the OSGi bundle URCAPS contribution
  *
@@ -10,12 +12,11 @@ import org.osgi.framework.BundleContext;
 public class Activator implements BundleActivator {
 	@Override
 	public void start(BundleContext bundleContext) throws Exception {
-		System.out.println("Activator says Hello World!");
+		bundleContext.registerService(SwingInstallationNodeService.class, new TestInstallationNodeService(), null);
 	}
 
 	@Override
 	public void stop(BundleContext bundleContext) throws Exception {
-		System.out.println("Activator says Goodbye World!");
 	}
 }
 
