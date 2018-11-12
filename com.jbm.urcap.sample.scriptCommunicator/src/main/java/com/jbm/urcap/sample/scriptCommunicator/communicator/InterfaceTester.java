4ca9049f9b7572a9aaf26b2018f8e53f55aa4542
jacobbsmadsen@gmail.com <jacobbsmadsen@gmail.com>
2018-11-12 13:52:22 +0100

    Added InterfaceTester class with main() for testing purposes

---
+++ com.jbm.urcap.sample.scriptCommunicator/src/main/java/com/jbm/urcap/sample/scriptCommunicator/communicator/InterfaceTester.java
@@ -0,0 +1,10 @@
+package com.jbm.urcap.sample.scriptCommunicator.communicator;
+
+public class InterfaceTester {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
