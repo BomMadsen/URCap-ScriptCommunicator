932ffeea8e2ea68868f8e6ad517d30d3fe2625a8
jacobbsmadsen@gmail.com <jacobbsmadsen@gmail.com>
2018-11-12 16:53:56 +0100

    Improved examples in InterfaceTester

--- com.jbm.urcap.sample.scriptCommunicator/src/main/java/com/jbm/urcap/sample/scriptCommunicator/communicator/InterfaceTester.java
+++ com.jbm.urcap.sample.scriptCommunicator/src/main/java/com/jbm/urcap/sample/scriptCommunicator/communicator/InterfaceTester.java
@@ -2,20 +2,40 @@ package com.jbm.urcap.sample.scriptCommunicator.communicator;
 
 public class InterfaceTester {
 
+	/**
+	 * This class can be used to test the Exporter and Sender
+	 * Hit the Play button in your IDE, to execute this
+	 * 
+	 */
+	
 	public static void main(String[] args) {
+		/**
+		 * Testing Sender
+		 */
+		ScriptSender sender = new ScriptSender();
+		
+		ScriptCommand senderCommand = new ScriptCommand("SenderCommand");
+		senderCommand.appendLine("textmsg(\"Add something to the logfile...\")");
+		
+		sender.sendScriptCommand(senderCommand);
+		
+		/**
+		 * Testing Exporter
+		 */
 		ScriptExporter export = new ScriptExporter();
 		
-		ScriptCommand commandString = new ScriptCommand("JacobsCommand1");
-//		commandString.appendLine("var_1 = \"Hello World\"");
+		ScriptCommand commandString = new ScriptCommand("Command1");
 		commandString.appendLine("pose = get_actual_tcp_pose()");
 		commandString.appendLine("z_value = pose[2]");
 		
-		export.exportStringFromURScript(commandString, "x_value");
+		String resultString = export.exportStringFromURScript(commandString, "z_value");
+		System.out.println("String result is: "+resultString);
 		
-		ScriptCommand commandInt = new ScriptCommand("JacobsCommand2");
-		commandInt.appendLine("var_2 = 42");
+		ScriptCommand commandInt = new ScriptCommand("Command2");
+		commandInt.appendLine("var_1 = 25 + 17");
 		
-		export.exportStringFromURScript(commandInt, "var_2");
+		int resultInt = export.exportIntegerFromURScript(commandInt, "var_1");
+		System.out.println("Integer result is: "+resultInt);
 	}
 
 }
